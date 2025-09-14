d70f8f78c2eb8b7f128523a4cd50f967ffb5a208
Besori <besoricompany@gmail.com>
2025-09-14 21:00:47 +0200

    Add files via upload

---
+++ includes/footer.js
@@ -0,0 +1,8 @@
+document.getElementById("footer-placeholder").innerHTML = `
+<footer role="contentinfo">
+    <span>© <span id="year"></span> Besori ®</span>
+    <span><a href="terms.html">Terms and Policies</a></span>
+    <span><a href="mailto:besoricompany@gmail.com">besoricompany@gmail.com</a></span>
+</footer>
+`;
+document.getElementById("year").textContent = new Date().getFullYear();
---
+++ includes/nav.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-placeholder").innerHTML = `
+<nav class="barra-nav" role="navigation" aria-label="Menú principal">
+    <a href="/index.html">Start</a>
+    <a href="/aboutus.html">About us</a>
+    <a href="#">Support</a>
+</nav>
+`;
